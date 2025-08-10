9c37a70cb3002cdeb7621d73e9bac9261c1cff79
Bn <adsunshine89@gmail.com>
2025-08-10 13:40:51 -0500

    Nav menu close bug

--- menuscript.js
+++ menuscript.js
@@ -5,3 +5,11 @@
  navLinks.classList.toggle('show');
     
  });
+
+// Close menu when any link is clicked
+const links = navLinks.querySelectorAll('li a'); // or 'li a' if links are inside <li>
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('show');
+  });
+});
